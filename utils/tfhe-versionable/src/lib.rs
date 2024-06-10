61bfeb6b87e0b73123446d0cb407b93d8f0f3553
Nicolas Sarlin <nicolas.sarlin@zama.ai>
2024-06-10 14:34:52 +0200

    feat(versionable): Add versionize support for aligned-vec types

--- utils/tfhe-versionable/src/lib.rs
+++ utils/tfhe-versionable/src/lib.rs
@@ -9,6 +9,7 @@
 pub mod derived_traits;
 pub mod upgrade;
 
+use aligned_vec::{ABox, AVec};
 use num_complex::Complex;
 use std::convert::Infallible;
 use std::fmt::Display;
@@ -331,3 +332,70 @@ impl<T: Unversionize> Unversionize for Complex<T> {
 }
 
 impl<T: NotVersioned> NotVersioned for Complex<T> {}
+
+impl<T: Versionize> Versionize for ABox<T> {
+    type Versioned<'vers> = T::Versioned<'vers> where T: 'vers;
+
+    fn versionize(&self) -> Self::Versioned<'_> {
+        self.as_ref().versionize()
+    }
+
+    type VersionedOwned = ABox<T::VersionedOwned>;
+
+    fn versionize_owned(&self) -> Self::VersionedOwned {
+        ABox::new(0, T::versionize_owned(self))
+    }
+}
+
+impl<T: Unversionize> Unversionize for ABox<T>
+where
+    T::VersionedOwned: Clone,
+{
+    fn unversionize(versioned: Self::VersionedOwned) -> Result<Self, UnversionizeError> {
+        Ok(ABox::new(0, T::unversionize((*versioned).to_owned())?))
+    }
+}
+
+impl<T: NotVersioned + Clone + Serialize + DeserializeOwned> Versionize for AVec<T> {
+    type Versioned<'vers> = &'vers [T] where T: 'vers;
+
+    fn versionize(&self) -> Self::Versioned<'_> {
+        self.as_slice()
+    }
+
+    type VersionedOwned = AVec<T>;
+
+    fn versionize_owned(&self) -> Self::VersionedOwned {
+        self.clone()
+    }
+}
+
+impl<T: NotVersioned + Clone + Serialize + DeserializeOwned> Unversionize for AVec<T> {
+    fn unversionize(versioned: Self::VersionedOwned) -> Result<Self, UnversionizeError> {
+        Ok(versioned)
+    }
+}
+
+impl<T: NotVersioned + Clone + Serialize + DeserializeOwned> NotVersioned for AVec<T> {}
+
+impl<T: NotVersioned + Clone + Serialize + DeserializeOwned> Versionize for ABox<[T]> {
+    type Versioned<'vers> = &'vers [T] where T: 'vers;
+
+    fn versionize(&self) -> Self::Versioned<'_> {
+        self.as_ref()
+    }
+
+    type VersionedOwned = ABox<[T]>;
+
+    fn versionize_owned(&self) -> Self::VersionedOwned {
+        self.clone()
+    }
+}
+
+impl<T: NotVersioned + Clone + Serialize + DeserializeOwned> Unversionize for ABox<[T]> {
+    fn unversionize(versioned: Self::VersionedOwned) -> Result<Self, UnversionizeError> {
+        Ok(versioned)
+    }
+}
+
+impl<T: NotVersioned + Clone + Serialize + DeserializeOwned> NotVersioned for ABox<[T]> {}
